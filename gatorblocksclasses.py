3998124046769c492dc5266b27d253c0c648c2fc
Daniel Todd <dgtutv@gmail.com>
2020-01-13 13:53:45 -0800

    User friendly classes
    allows anyone to use the app

--- gatorblocksclasses.py
+++ gatorblocksclasses.py
@@ -1,2 +1,7 @@
-#please enter classes in order from 1-1 to 2-4
-classes =['AP Comp Sci','Math','Chemistry','Spare','Spare','Physics','Programming','English']
+import json
+print("please enter classes in order from 1-1 to 2-4")
+classes = []
+for i in range(8):
+    classes+=[str(input())]
+with open('classes.txt', 'w') as filehandle:
+    json.dump(classes, filehandle)
