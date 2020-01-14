a574eabce44e8f63b4275da69adf3fa2668286d7
Daniel Todd <dgtutv@gmail.com>
2020-01-14 14:37:17 -0800

    Update referenceDays.py

--- referenceDays.py
+++ referenceDays.py
@@ -15,9 +15,9 @@ month= int(input('Month: '))
 day= int(input('Day: '))
 date1=datetime.date(year, month, day)
 day_of_year = date1.timetuple().tm_yday
-end=day_of_year
+Final=day_of_year
 with open('referenceFinal.txt', 'w') as filehandle:
-    json.dump(end, filehandle)
+    json.dump(Final, filehandle)
 print("If there is a weekend between the new year and first day of school after the winter break, enter 2. Otherwise enter 0")
 end=int(input())
 with open('referenceEnd.txt', 'w') as filehandle:
