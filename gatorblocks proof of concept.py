e733d5052c252af5bf0c0505c456f0b8c76c91e8
Daniel Todd <dgtutv@gmail.com>
2020-01-29 21:20:22 -0800

    This is a major overhaul to the algorithm
    This overhaul still does not work but is definitely a step in the right direction when it comes to future proofing

--- gatorblocks proof of concept.py
+++ gatorblocks proof of concept.py
@@ -14,108 +14,60 @@ with open('referenceEnd.txt', 'r') as filehandle:
     end = json.load(filehandle)
 with open('referenceFinal.txt', 'r') as filehandle:
     Final = json.load(filehandle)
+with open('referenceLastDay.txt', 'r') as filehandle:
+    LastDay = json.load(filehandle)
+with open('referenceFirstDay.txt', 'r') as filehandle:
+    FirstDay = json.load(filehandle)    
 end = (365-begin)+end
-day_of_year = datetime.now().timetuple().tm_yday#day of year from date
-now = datetime.now()#current date
-week = calendar.weekday(now.year, now.month, now.day)#get day of week
+day_of_year = datetime.now().timetuple().tm_yday   #day of year from date
+now = datetime.now()  #current date
+week = calendar.weekday(now.year, now.month, now.day)   #get day of week
 day1 = [0,1,2,3]
 day2 = [4,5,6,7]
 day3 = [2,3,0,1]
-day4 = [6,7,4,5]
+day4 = [6,7,4,5]        #These are my block rotations
 day5 = [1,0,3,2]
 day6 = [5,4,7,6]
 day7 = [3,2,1,0]
 day8 = [7,6,5,4]
-weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-masterlist = [day1,day2,day3,day4,day5,day6,day7,day8]
+weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]    #Used for assigning a number to days of the week, mostly useful for determining at which point the block rotations should start
+masterlist = [day1,day2,day3,day4,day5,day6,day7,day8]     #This list is used for rotating through the block rotations
 #247, first day of scool
 #176, days in school year
-#120 between start end year
-#421 in my sceduale
-w=0
-day_off =[]
-for offDay in dayOff:#from list of days off in day of year format, converts to day of school year    
-    if dayOff[w] >=begin:
-        day_off += [dayOff[w]-int(begin)]
-    else:
-        day_off += [dayOff[w]+int(end)]
-    w+=1
-def before():
-    dayOfYear=day_of_year-int(begin) #converts current day to day of school year
-    rotation =1
-    b=0#a counter for total days that have been ran through the loop
-    for day in range(365):
-        rotation+=1
-        if rotation ==-1:
-            continue
-        if rotation ==5:
-            rotation = -2
-            continue
-        if day in day_off:
-            day+=1
-            continue
-        b+=1
-        dayz = (b%8)-1
-        x = masterlist[dayz] 
-        if day== dayOfYear:
-            return x
-            break
-    
-def after():
-    dayOfYear=day_of_year+end#converts current day to day of school year
-    rotation =-1
-    b=-1#a counter for total days that have been ran through the loop
-    for day in range(365):
-        rotation+=1
-        if rotation ==-1:
-            continue
-        if rotation ==5:
-            rotation = -2
-            continue
-        if day in day_off:
-            day+=1
-            continue
-        b+=1
-        dayz = (b%8)-1
-        x = masterlist[dayz] 
-        if day== dayOfYear:
-            return x
-            break
 
+day_of_year=249   #debugging date
+
+w=0     #counter for running through and iterating list of days off
+counter=0     #counter for the number of days running through the main for loop, not including weekends or days off
+if day_of_year<Final-1:  #Final is the dev inputted number that inicates the last day of school
+    day_of_year+=365
+day_of_year-=begin      
+for offDay in dayOff:    #from list of days off in day of year format, converts to day of school year    
+    if dayOff[w] <Final-1:    
+        dayOff[w]+=365    #This is similar to the if statement above, just appplies to a list
+    dayOff[w]-=begin  #begin is a dev inputted first day of block rotation
+rotation=(weekDay.index(FirstDay))
 print(weekDay[week])
 print()
-if day_of_year>=Final:
-    if now.weekday()<5:        
-        p =day_of_year-Final
-    else:
-        p=5
-    if p==0:
-        print(classes[day1[0]])
-        print(classes[day1[1]])
-    elif p==1:
-        print(classes[day1[2]])
-        print(classes[day1[3]])
-    elif p==2:
-        print(classes[day2[0]])
-        print(classes[day2[1]])
-    elif p==3:
-        print(classes[day2[2]])
-        print(classes[day2[3]])
-else:
-    if day_of_year>=(begin-1):
-        x = before()
-    else:
-        x = after()
-    for i in range(4):
-        y = x[i]
-        p = classes[y]
-        print(p)
-    print()
-input("Press ENTER to end program")
-     
- 
+for day in range(365):      #iterates through every day from the start of school until today, this is because we want to use modulo to tell which block rotation should be used
+    #even if not a school day we still want to display day of week
+    rotation+=1
+    if rotation ==-1:
+        continue
+    if rotation ==5:        #This code skips weekends
+        rotation=-2
+        continue
+    if day in dayOff:
+        continue
+    dayz=(counter%8)        #This code skips dev entered days off school
+    counter+=1
+    x=masterlist[dayz]
+    if day == day_of_year:      #When the code reaches to todays date, it will use modulo to tell which rotation is appropiate and then will cycle through the afformentioned lists to display today's classes
+        for i in range(4):
+            y = x[i]
+            p = classes[y]
+            print(p)
+    break
+print()
+input("Press ENTER to end program")     #Windows will not run without this 
 
-
-    
-    
-    
